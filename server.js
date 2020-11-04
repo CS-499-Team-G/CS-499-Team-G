f90ad11fb900606812f9900dee8dcb748efdc4fa
dpeaks <dante.peaks17@gmail.com>
2020-11-03 19:13:51 -0600

    The server will serve home.html and then look in the pages folder to use other html and js files

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json()); // This will let us analyze the body of a request
 
+app.use(express.static('./pages'));
+
 require('dotenv').config();
 
 // Gives access to the data models
@@ -29,16 +31,16 @@ connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
 })
 
-
-
 /* Get requests are used to get information from the server. 
    Once we receive a request, we will send information back.
-
+*/
 app.get('*', (req, res) =>
-    res.sendFile('home.html')
+    /* Server.js lives in the root directory. _dirname is an 
+       environment var that returns the current working dir.
+    */
+    res.sendFile(path.resolve(__dirname, 'home.html'))
 );
 
-*/
 app.listen(PORT, () => 
     console.log(`Server started on port ${PORT}`)
 );
