08be0107075d312dbb2c54607b7810ef7a159946
dpeaks <dante.peaks17@gmail.com>
2020-11-04 21:21:50 -0600

    Changed function to serve pages folder

--- server.js
+++ server.js
@@ -11,8 +11,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json()); // This will let us analyze the body of a request
 
-//app.use(express.static('./pages'));
-app.use('pages', express.static('server'));
+app.use(express.static('pages'));
 
 // Gives access to the data models
 const usersRouter = require('./middleware/routes/users'); // .require() lets us use the api calls in the Users.js
@@ -41,11 +40,11 @@ app.get('/', (req, res) =>
     */
     res.sendFile(path.resolve(__dirname, 'home.html'))
 );
-/*
+
 app.get('/pages/sign-up.html', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'pages', 'sign-up.html'))
 );
-*/
+
 app.listen(PORT, () => 
     console.log(`Server started on port ${PORT}`)
 );
