7f04321be7ee10916c04e4e86367ab70c8b2787f
Cameron Howell <crh0043@uah.edu>
2020-11-16 09:40:18 -0600

    Changed folder structure

---
+++ pages/js/reports.js
@@ -0,0 +1,24 @@
+function fetchData(){
+    fetch("http:localhost:5000/users/login")
+        .then(response =>{
+            console.log(response.body);
+            if(!response.ok){
+                throw Error("ERROR");
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data.data);
+            const html = data.data.map(user => {
+                return `<p>Name: ${user.userName}</p>`;
+            });
+        })
+        .catch(error => {
+            console.log(error);
+        });
+}
+function msg() {
+    alert("Request: "+ response.body);
+  }
+
+msg();
