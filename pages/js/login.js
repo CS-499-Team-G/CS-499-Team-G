653fc1d663c7305308263e7124342be21badf9cd
Cameron Howell <crh0043@uah.edu>
2020-11-19 19:19:29 -0600

    Still trying

--- pages/js/login.js
+++ pages/js/login.js
@@ -1,11 +1,8 @@
 // login.js
 function login() {
-	username = $("#username").val();
-	password = $("#password").val();
-
 	sessionStorage.setItem("AuthenticationState", "Authenticated");
 
 	sessionStorage.setItem("AuthenticationExpires", Date.now.addHours(1));
 
-	location.href = "reports.html";
+	window.location.href = "../reports.html";
 }
