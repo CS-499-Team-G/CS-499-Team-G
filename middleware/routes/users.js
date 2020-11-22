aa10d03a284049d23026f477fda1401cace3ed09
dpeaks <dante.peaks17@gmail.com>
2020-11-22 16:12:01 -0600

    Returns only the assingment of the user

--- middleware/routes/users.js
+++ middleware/routes/users.js
@@ -73,7 +73,7 @@ router.route('/assignment').post((req, res) => {
 
   User.findOne({userName: username})
   .populate('assignment')
-    .then(assignments => res.json(assignments))
+    .then(user => res.json(user.assignment))
     .catch(err => res.status(400).json('Error: ' + err));
 });
  
