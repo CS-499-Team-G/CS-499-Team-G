bfb598200789ea233783837dd50a79544ba72100
Cameron Howell <crh0043@uah.edu>
2020-11-23 21:44:06 -0600

    Once more

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json()); // This will let us analyze the body of a request
-app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static("pages"));
 
