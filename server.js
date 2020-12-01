ecc55380e5174169de2968aab7f403e0d2500638
Cameron Howell <crh0043@uah.edu>
2020-11-30 18:48:20 -0600

    Concurrent with feature branch

--- server.js
+++ server.js
@@ -9,9 +9,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express.json()); // This will let us analyze the body of a request
+app.use(express.json()); // This will let us analyze the body of a JSON request
+app.use(express.urlencoded({ extended: false })); // This will let us analyze the body of any other type of request
 
-app.use(express.static("pages"));
+app.use(express.static(path.join(__dirname, "pages")));
 
 // Gives access to the data models
 const usersRouter = require("./middleware/routes/users"); // .require() lets us use the api calls in the Users.js
@@ -37,6 +38,8 @@ connection.once("open", () => {
 /* Get requests are used to get information from the server. 
    Once we receive a request, we will send information back.
 */
+
+// API Call: http:localhost5000/
 app.get("/", (req, res) =>
 	/* Server.js lives in the root directory. _dirname is an 
        environment var that returns the current working dir.
@@ -44,6 +47,7 @@ app.get("/", (req, res) =>
 	res.sendFile(path.resolve(__dirname, "home.html"))
 );
 
+// API Call: http:localhost5000/pages/sign-up.html
 app.get("/pages/sign-up.html", (req, res) =>
 	res.sendFile(path.resolve(__dirname, "pages", "sign-up.html"))
 );
