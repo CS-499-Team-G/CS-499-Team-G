2c10f842f08bfaee6af7e5e5f1ca489230fb9ab7
Cameron Howell <crh0043@uah.edu>
2020-11-30 19:05:41 -0600

    Maybe fixed???

--- server.js
+++ server.js
@@ -10,8 +10,9 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json()); // This will let us analyze the body of a request
+app.use(express.urlencoded({ extended: false }));
 
-app.use(express.static("pages"));
+app.use(express.static(path.join(__dirname, "pages")));
 
 // Gives access to the data models
 const usersRouter = require("./middleware/routes/users"); // .require() lets us use the api calls in the Users.js
