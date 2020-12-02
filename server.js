a7d1215f44292e1b846d073d8001fb9534b81dc3
Cameron Howell <crh0043@uah.edu>
2020-12-01 21:56:00 -0600

    Squashed and merged commits from cameron branch

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express.json()); // This will let us analyze the body of a JSON request
-app.use(express.urlencoded({ extended: false })); // This will let us analyze the body of any other type of request
+app.use(express.json()); // This will let us analyze the body of a request
+app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, "pages")));
 
@@ -51,5 +51,9 @@ app.get("/", (req, res) =>
 app.get("/pages/sign-up.html", (req, res) =>
 	res.sendFile(path.resolve(__dirname, "pages", "sign-up.html"))
 );
+
+app.get("*", function (req, res) {
+	res.send("Error 404: Page not found", 404);
+});
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
