b9ebf18c9ad23f0926af624f7200560f011b0972
Cameron Howell <crh0043@uah.edu>
2020-12-02 23:40:16 -0600

    Test of incoming shipment

--- pages/reports.js
+++ pages/reports.js
@@ -1,15 +1,11 @@
 function getData() {
 	var req = new XMLHttpRequest();
-	req.open("GET", "http://68.93.20.191:5000/shipments/", false);
-	req.send(null);
-
-	var jsonObj = JSON.parse(req.responseText);
-	shipmentTable(jsonObj);
-}
-
-function reportDecision() {
 	if (document.getElementById("selection").value === "3") {
-		shipmentTable();
+		req.open("GET", "http://68.93.20.191:5000/shipments/incoming", false);
+		req.send(null);
+
+		var jsonObj = JSON.parse(req.responseText);
+		shipmentTable(jsonObj);
 	}
 }
 
@@ -19,26 +15,28 @@ function shipmentTable(data) {
 	// Create header elements and initial header row
 	var headerRow = document.createElement("tr");
 	var traffic = document.createElement("th");
-	var company = document.createElement("th");
-	var address = document.createElement("th");
+	var origin = document.createElement("th");
+	var destination = document.createElement("th");
 	var vim = document.createElement("th");
 	var depart = document.createElement("th");
 	var arrive = document.createElement("th");
 	var status = document.createElement("th");
+	var payment = document.createElement("th");
 
 	// Populate header elements with text nodes
 	traffic.appendChild(document.createTextNode("Traffic"));
-	company.appendChild(document.createTextNode("Company"));
-	address.appendChild(document.createTextNode("Address"));
+	origin.appendChild(document.createTextNode("Origin"));
+	destination.appendChild(document.createTextNode("Destination"));
 	vim.appendChild(document.createTextNode("Vehicle ID"));
 	depart.appendChild(document.createTextNode("Departure Date"));
 	arrive.appendChild(document.createTextNode("Arrival Date"));
 	status.appendChild(document.createTextNode("Status"));
+	payment.appendChild(document.createTextNode("Paid"));
 
 	// Append the header elements to the row element
 	headerRow.appendChild(traffic);
-	headerRow.appendChild(company);
-	headerRow.appendChild(address);
+	headerRow.appendChild(origin);
+	headerRow.appendChild(destination);
 	headerRow.appendChild(vim);
 	headerRow.appendChild(depart);
 	headerRow.appendChild(arrive);
@@ -47,12 +45,36 @@ function shipmentTable(data) {
 	// Append the row to the table
 	table.appendChild(headerRow);
 
-	var dataRow = document.createElement("tr");
-	var cell1 = document.createElement("td");
+	for (let i = 0; i < jsonObj.length; i++) {
+		// Create row element
+		var row = document.createElement("tr");
 
-	cell1.appendChild(document.createTextNode(data[0].traffic));
+		// Create each data field element
+		var cell1 = document.createElement("td");
+		var cell2 = document.createElement("td");
+		var cell3 = document.createElement("td");
+		var cell4 = document.createElement("td");
+		var cell5 = document.createElement("td");
+		var cell6 = document.createElement("td");
+		var cell7 = document.createElement("td");
+		var cell8 = document.createElement("td");
 
-	dataRow.appendChild(cell1);
-
-	table.appendChild(dataRow);
+		// Populate each data field with a text node
+		cell1.appendChild(document.createTextNode(data[i].traffic));
+		cell2.appendChild(document.createTextNode(data[i].origin.oCompany));
+		cell3.appendChild(document.createTextNode(data[i].destination.dCompany));
+		cell4.appendChild(document.createTextNode("N/A"));
+		cell5.appendChild(document.createTextNode(data[i].departureDate));
+		cell6.appendChild(document.createTextNode(data[i].arrivalDate));
+		if (data[i].arrivalStatus) {
+			cell7.appendChild(document.createTextNode("Arrived"));
+		} else {
+			cell7.appendChild(document.createTextNode("In Process"));
+		}
+		if (data[i].payment) {
+			cell8.appendChild(document.createTextNode("Yes"));
+		} else {
+			cell8.appendChild(document.createTextNode("No"));
+		}
+	}
 }
