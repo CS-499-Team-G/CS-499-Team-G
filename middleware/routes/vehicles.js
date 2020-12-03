442d06298b4d0e185bc4b9790ecae55c3f4dcbff
dpeaks <dante.peaks17@gmail.com>
2020-12-03 11:01:29 -0600

    Removed extra add function

--- middleware/routes/vehicles.js
+++ middleware/routes/vehicles.js
@@ -15,38 +15,6 @@ router.route('/maintenance').post((req, res) => {
     .catch(err => res.status(400).json('Error: ' + err));
 })
 
-router.route('/add').post((req, res) => 
-{
-    const brand = req.body.brand;
-    const year = req.body.year;
-    const model = req.body.model;
-    const kind = req.body.kind;
-    const shipment = req.body.shipment;
-
-    const date = req.body.date;
-    const inspections = req.body.inspections;
-    const inspectionsRecords = {inspections, date};
-
-    const description = req.body.description;
-    const parts = req.body.parts;
-    const supplier = req.body.supplier;
-    const repairRecords = {description, parts, supplier};
-
-    const maintenance = req.body.maintenance;
-    const maintenanceRecord = {maintenance, repairRecords, inspectionsRecords}; 
-
-    console.log(req.body);
-
-const newVehicle = new Vehicles({
-
-    brand,
-    year,
-    model,
-    kind,
-    shipment,
-    maintenanceRecord
-});
-
 router.route("/add").post((req, res) => {
 	const brand = req.body.brand;
 	const year = req.body.year;
