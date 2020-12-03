e8e9c7f81c70e72c6d18d7118c4d61551d3180c7
Cameron Howell <crh0043@uah.edu>
2020-12-03 01:33:32 -0600

    Hm

--- pages/login.js
+++ pages/login.js
@@ -1,13 +1,12 @@
 function login() {
 	var req = new XMLHttpRequest();
-	req.open(
-		"POST",
-		"http://68.93.20.191:5000/users/login",
-		false,
-		document.getElementById("username").value,
-		document.getElementById("password").value
+	req.open("POST", "http://68.93.20.191:5000/users/login", false);
+	req.send(
+		"username=" +
+			document.getElementById("username").value +
+			"&password=" +
+			document.getElementById("password").value
 	);
-	req.send(null);
 
 	if (req.responseText === "Valid") {
 		window.location.href = "reports.html";
