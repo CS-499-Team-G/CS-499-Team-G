cc9ec3be5552c2b49938b421c1eee1fc81b739a8
Cameron Howell <crh0043@uah.edu>
2020-12-03 13:10:21 -0600

    One more try

--- pages/reports.js
+++ pages/reports.js
@@ -45,7 +45,7 @@ function payrollTable(data) {
 	// Create header elements and initial header row
 	var headerRow = document.createElement("tr");
 	var name = document.createElement("th");
-	name.onclick = "sortTable(0)";
+	name.addEventListener("click", sortTable(0));
 	var title = document.createElement("th");
 	var payrate = document.createElement("th");
 	var tenure = document.createElement("th");
