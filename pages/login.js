d97bd8ab8df3a5b755121c348814ac6b30c68b28
Cameron Howell <crh0043@uah.edu>
2020-12-03 01:53:09 -0600

    idek

--- pages/login.js
+++ pages/login.js
@@ -1,7 +1,7 @@
-function login(data) {
+function login(username, password) {
 	var req = new XMLHttpRequest();
 	req.open("POST", "http://68.93.20.191:5000/users/login", false);
-	req.send(data);
+	req.send("{ username: " + username + ", password: " + password + " }");
 
 	if (req.responseText === "Valid") {
 		window.location.href = "reports.html";
