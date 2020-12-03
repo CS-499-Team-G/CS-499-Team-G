f17c439e5f45526bdd5e1f074028d731dbc5d551
Cameron Howell <crh0043@uah.edu>
2020-12-03 01:42:59 -0600

    test of req body

--- pages/login.js
+++ pages/login.js
@@ -2,7 +2,7 @@ function login() {
 	var req = new XMLHttpRequest();
 	req.open("POST", "http://68.93.20.191:5000/users/login", false);
 	req.send(
-		"username=" +
+		"?username=" +
 			document.getElementById("username").value +
 			"&password=" +
 			document.getElementById("password").value
