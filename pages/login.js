ef5c5ca9054036392683e55594009e96bebc9d30
Cameron Howell <crh0043@uah.edu>
2020-12-03 01:46:29 -0600

    what am I even doing

--- pages/login.js
+++ pages/login.js
@@ -1,14 +1,7 @@
 function login(data) {
 	var req = new XMLHttpRequest();
 	req.open("POST", "http://68.93.20.191:5000/users/login", false);
-	req.send(
-		"?username=" +
-			document.getElementById("username").value +
-			"&password=" +
-			document.getElementById("password").value
-	);
-
-	console.log(data);
+	req.send(data);
 
 	if (req.responseText === "Valid") {
 		window.location.href = "reports.html";
