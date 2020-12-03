df742e33db77452c9d0b80d59ed46160ccf9e623
Cameron Howell <crh0043@uah.edu>
2020-12-02 23:56:06 -0600

    Updated user.js with login route

--- middleware/routes/users.js
+++ middleware/routes/users.js
@@ -1,24 +1,87 @@
+const express = require("express");
+const mongoose = require("mongoose");
 const router = require("express").Router();
-let User = require("../models/users.model");
+let User = require("../models/users.model.js");
+const path = require("path"); // Needed so that we can serve pages
+const app = express();
+//app.use(express.static(path.join(__dirname, "/../../pages")));
+
 const { check, validationResult } = require("express-validator"); // Allows us to use the express-validator to validate data from webpage https://express-validator.github.io/docs/
 
 /*  .get == > retrieve the data from the webpage without altering
     .post ==> retrieve the data from the webpage and alter
 */
 
-router.route("/").get((req, res) => {
-	const username = req.body.username;
-	const title = req.body.title;
-	res.send("User route!");
+/**
+ * Summary. Check if the provided username exists. This function
+ * returns an array containing all the infornation of every user.
+ * @param array Mongodb query returns an array containing the
+ * users with a given user name.
+ */
+function checkArray(queryResult, type, res) {
+	console.log("Check array function");
+	if (type == "users") {
+		model = "User";
+	} else {
+		model = "Assignment";
+	}
+	if (!queryResult.length == 0) {
+		//res.json(queryResult)
+		app.use(express.static(path.join(__dirname, "/../../pages")));
+		console.log("Dirname: " + __dirname);
+		res.sendFile(path.resolve(__dirname, "..", "..", "pages", "reports.html"));
+	} else {
+		throw "Query returned 0 results. Incorrect information entered.";
+	}
+}
 
-	/*User.find()
-    .then(users => res.json(users))
-    .catch(err => res.status(400).json('Error: ' + err));*/
+// Login for a specific user
+router.route("/login").post((req, res) => {
+	// Get the username and password from the req.body
+	const { username, password } = req.body;
+
+	// Log to the console to see if we are receiving requests
+	console.log(req.body);
+
+	/**
+	 * Summary. Check if the provided username exists. This function
+	 * returns an array containing all the infornation of every user.
+	 * @param userName name of a user account from a webpage request.
+	 */
+	User.find({ userName: username, password: password })
+		.then((users) => checkArray(users, "users", res))
+		.catch((err) => res.status(400).json("Error: " + err));
 });
 
+// Show all users
+router.route("/").get((req, res) => {
+	User.find()
+		.then((users) => res.json(users))
+		.catch((err) => res.status(400).json("Error: " + err));
+});
+
+// Show payroll for all users
+router.route("/payroll").post((req, res) => {
+	User.find({}, { payRate: 1, fullName: 1, title: 1 })
+		.then((users) => res.json(users))
+		.catch((err) => res.status(400).json("Error: " + err));
+});
+
+// Show user work assignment
+router.route("/assignment").post((req, res) => {
+	const username = req.body.username;
+
+	User.findOne({ userName: username })
+		.populate("assignment")
+		.then((user) => res.json(user.assignment))
+		.catch((err) => res.status(400).json("Error: " + err));
+});
+
+// Create a user and add to the database
 router.route("/add").post((req, res) => {
 	const userName = req.body.username;
 	const title = req.body.title;
+	const password = req.body.password;
 
 	const firstName = req.body.firstName;
 	const lastName = req.body.lastName;
@@ -33,6 +96,7 @@ router.route("/add").post((req, res) => {
 
 	const payRate = req.body.payRate;
 	const tenure = req.body.tenure;
+	const assignment = req.body.assignment;
 
 	// Log to the console to see if we are receiving requests
 	console.log(req.body);
@@ -40,12 +104,15 @@ router.route("/add").post((req, res) => {
 	const newUser = new User({
 		userName,
 		title,
-		fullName, //: {firstName: firstName, middleName: middleName, lastName: lastName}
+		password,
+		fullName,
 		address,
 		payRate,
 		tenure,
+		assignment,
 	});
 
+	// Save the newly created user in Mongodb
 	newUser
 		.save()
 		.then(() => res.json("User added!"))
