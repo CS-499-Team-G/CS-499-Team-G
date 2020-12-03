c96f2992d6588c65ca361c91fe8bd5cee5535541
Cameron Howell <crh0043@uah.edu>
2020-12-03 02:05:34 -0600

    Fixed JSON parse error

--- pages/login.js
+++ pages/login.js
@@ -1,7 +1,9 @@
 function login(user, pass) {
 	var req = new XMLHttpRequest();
 	req.open("POST", "http://68.93.20.191:5000/users/login", false);
-	var data = JSON.parse("{ username: " + user + ", password: " + pass + " }");
+	var data = JSON.parse(
+		'{ "username": "' + user + '", "password": "' + pass + '" }'
+	);
 	req.send(data);
 
 	console.log(req.body);
