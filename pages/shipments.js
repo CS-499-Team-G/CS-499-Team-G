31b5d3eed8f633fcb5ad6476754cab027c147cf2
dpeaks <dante.peaks17@gmail.com>
2020-12-08 20:48:43 -0600

    Some work

---
+++ pages/shipments.js
@@ -0,0 +1,10 @@
+function addShipment(){
+	var req = new XMLHttpRequest();
+
+    req.open("POST", "http://68.93.20.191:5000/shipments/add", false);
+		req.send(null);
+
+		var newVehicle = JSON.parse(req.responseText);
+		//payrollTable(payrollJson);
+
+}
